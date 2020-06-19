59e0bc40c6ce454d7abbdcfe175827c344b05d9b
Your Name <you@example.com>
2020-06-19 16:49:51 -0300

    Aula21Correçãoex20

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula20_21_Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cliente lucca = new Cliente("luccao");
+            lucca.Endereco = "Av. espacial, 102";
+            Restaurante mcDonalds = new Restaurante("McDonalds", "Av Paulista, 123");
+
+            Pedido pedido1 = new Pedido();
+            pedido1.Cliente = lucca;
+            pedido1.Restaurante = mcDonalds;
+
+            Console.WriteLine( pedido1.EntregarPedido() );
+
+        }
+    }
+}
